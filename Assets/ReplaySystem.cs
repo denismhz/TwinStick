32bb863a4ffc0a497705f6420a1babd41fcbf735
Denis Manherz <denis.manherz@gmx.de>
2018-02-26 19:53:27 +0100

    Adding ReplaySystem

---
+++ Assets/ReplaySystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+    private const int bufferFrames = 100;
+    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        MyKeyFrame keyFrame = new MyKeyFrame(1.0f, Vector3.up, Quaternion.identity);
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Record();
+	}
+
+    void PlayBack()
+    {
+        rigidBody.isKinematic = true;
+        int frame = Time.frameCount % bufferFrames;
+        Debug.Log("Reading frame: " + frame);
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+    }
+
+    void Record()
+    {
+        rigidBody.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+        float time = Time.time;
+        Debug.Log("Writing frame: " + frame);
+
+        keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+    }
+}
+
+public struct MyKeyFrame
+{
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame(float aTime, Vector3 pos, Quaternion rot)
+    {
+        frameTime = aTime;
+        position = pos;
+        rotation = rot;
+    }
+}
